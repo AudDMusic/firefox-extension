4aa6c9534ba9f2ded8b902c6b7fe60b96ce0716a
Mikhail Samin <i@mishasamin.xyz>
2025-07-16 16:07:12 +0100

    Update background.js

--- background.js
+++ background.js
@@ -319,30 +319,6 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 				//		email = user_info["email"];
 				//		google_id = user_info["id"];
 				}
-				var a = function(key, str) {
-					str = atob(str);
-					var s = [], j = 0, x, res = '';
-					for (var i = 0; i < 256; i++) {
-						s[i] = i;
-					}
-					for (i = 0; i < 256; i++) {
-						j = (j + s[i] + key.charCodeAt(i % key.length)) % 256;
-						x = s[i];
-						s[i] = s[j];
-						s[j] = x;
-					}
-					i = 0;
-					j = 0;
-					for (var y = 0; y < str.length; y++) {
-						i = (i + 1) % 256;
-						j = (j + s[i]) % 256;
-						x = s[i];
-						s[i] = s[j];
-						s[j] = x;
-						res += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
-					}
-					return btoa(res);
-				};
 				var info = {
 				"tab_url": tab_url,
 				"email": email,
